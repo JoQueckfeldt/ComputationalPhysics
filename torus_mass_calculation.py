e4747f9b583d0f8ea6fc953b21787b52c218165a
Jonathan Queckfeldt <jonte.queckfeldt@icloud.com>
2025-02-10 11:21:39 +0100

    extended function for Bode's rule to composite Bode's rule

--- torus_mass_calculation.py
+++ torus_mass_calculation.py
@@ -2,6 +2,15 @@ import numpy as np
 import scipy.integrate as spi
 import matplotlib.pyplot as plt
 
+
+def f(t):
+    """
+    Integrand function for the mass of a torus in terms of t, 
+    derived from washer method.
+    """
+    return np.sqrt(1 - t**2)
+
+
 def legendre_polynomial(n):
     """Compute the Legendre polynomial P_n(x)"""
     if n == 0:
@@ -19,7 +28,7 @@ def legendre_polynomial(n):
     return Pn
 
 
-def gauss_legendre_quadrature(n, func):
+def gauss_legendre(n, func=f):
     """
     Compute the integral of a function over the interval [-1, 1] 
     using Gauss-Legendre quadrature of order n.
@@ -30,6 +39,8 @@ def gauss_legendre_quadrature(n, func):
 
     Returns:
     float, the integral of the function
+    array, nodes
+    array, weights
     """
 
     # Get Legendre polynomial P_n(x) and its derivative P'_n(x)
@@ -42,23 +53,38 @@ def gauss_legendre_quadrature(n, func):
     # Compute the weights
     w = 2 / ((1-nodes**2) * (Pn_derivative(nodes)**2))
     
-    return np.sum(w*func(nodes))
+    return np.sum(w*func(nodes)), nodes, w
 
 
-def f(t):
+def bode_rule(n, func=f):
+    """ 
+    Compute the integral of a function over the interval [-1, 1]
+    using the composite Bode's rule of order n.
+
+    Parameters:
+    n: int, order of the quadrature
+    f: callable, function to be integrated
+
+    Returns:
+    float, the integral of the function
+    array, nodes
+    array, weights
     """
-    Integrand function for the mass of a torus in terms of t, 
-    derived from washer method.
-    """
-    return np.sqrt(1 - t**2)
-
-
-def bode_rule(n):
     x = np.linspace(-1, 1, n + 1)
-    y = f(x)
+    y = func(x)
     h = (x[-1] - x[0]) / n
-    integral = (2*h/45) * (7*y[0] + 32*np.sum(y[1:-1:2]) + 12*np.sum(y[2:-2:4]) + 32*np.sum(y[3:-1:4]) + 7*y[-1])
-    return integral
+    
+    # Bode's rule weights
+    weights = np.zeros_like(x)
+    weights[::n] = 7
+    weights[1:-1:2] = 32 
+    weights[2:-2:4] = 12 
+    weights[4:-4:4] = 14
+    weights *= 2 * h / 45
+
+    integral = np.sum(weights * y)
+
+    return integral, x, weights
 
 
 def main():
@@ -84,10 +110,10 @@ def main():
     for n in n_values:
         x = np.linspace(-1, 1, n+1)
 
-        glq_error.append(np.abs(C*gauss_legendre_quadrature(n, f) - M_exact))
+        glq_error.append(np.abs(C*gauss_legendre(n)[0] - M_exact))
         simpson_error.append(np.abs(C*spi.simpson(f(x), x) - M_exact))
         if n%4 == 0: # N must be multiple of 4 for Bode's rule
-            bode_error.append(np.abs(C*bode_rule(n) - M_exact))
+            bode_error.append(np.abs(C*bode_rule(n)[0] - M_exact))
 
     plt.figure()
     plt.plot(n_values, glq_error, marker='o', label='Gauss-Legendre')
